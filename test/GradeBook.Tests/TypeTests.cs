5cd89f78a972200456b7026efe38deecee4050a7
Wojciech Dudek <wdudek82@gmail.com>
2020-04-22 21:10:50 +0200

    Add new tests to TypeTests.cs

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -1,3 +1,5 @@
+using System.Transactions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Xunit;
 
 namespace GradeBook.Tests
@@ -10,10 +12,10 @@ namespace GradeBook.Tests
             // Given
             const string name1 = "Book 1";
             const string name2 = "Book 2";
-
-            // When
             var book1 = GetBook(name1);
             var book2 = GetBook(name2);
+
+            // When
 
             // Then
             Assert.Equal(name1, book1.Name);
@@ -25,18 +27,79 @@ namespace GradeBook.Tests
         {
             // Given
             const string name1 = "Book 1";
-
-            // When
             var book1 = GetBook(name1);
             var book2 = book1;
+
+            // When
 
             // Then
             Assert.Same(book1,  book2);
         }
 
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            // Given
+            const string name1 = "Book 1";
+            const string name2 = "New Name";
+            var book1 = GetBook(name1);
+            var book2 = book1;
+
+            // When
+            SetName(book1, name2);
+
+            // Then
+            // Assert.Same(book1, book2);
+            Assert.Equal(name2, book1.Name);
+        }
+
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            // Given
+            const string name1 = "Book1";
+            const string name2 = "New Name";
+            var book1 = GetBook(name1);
+
+            // When
+            GetBookSetName(book1, name2);
+
+            // Then
+            Assert.Equal(name1, book1.Name);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var x = GetInt3();
+            SetInt(ref x);
+
+            Assert.Equal(4, x);
+        }
+
+        private static void SetInt(ref int x)
+        {
+            x = 4;
+        }
+
+        private static int GetInt3()
+        {
+            return 3;
+        }
+
         private static Book GetBook(string name)
         {
             return new Book(name);
+        }
+
+        private static void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+
+        private static void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
         }
     }
 }
